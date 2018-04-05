2a9fab406b8b4a18627b9f9ca4a09b49c197750a
Yury Trafimovich <Yury_Trafimovich@epam.com>
2018-04-05 10:39:28 +0300

    use functinal component for Label

--- src/formElements/Label.js
+++ src/formElements/Label.js
@@ -1,32 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import labelStore from '../store/Label';
 import './Label.css';
 
-export default class Label extends Component {
-    static propTypes = {
-        for: PropTypes.string,
-        children: PropTypes.oneOfType([
-            PropTypes.string,
-            PropTypes.node
-        ])
-    };
-
-    handleOnClick = () => {
-        const { for: for_props } = this.props;
+const Label = ({ for: for_props, children }) => {
+    const handleOnClick = () => {
         if (for_props !== undefined) {
             labelStore.emit(`onclick_${for_props}`);
         }
     };
 
-    render() {
-        return (
-            <span
-                className="Label"
-                onClick={this.handleOnClick}
-            >
-                {this.props.children}
-            </span>
-        )
-    }
-}
+    return (
+        <span
+            className="Label"
+            onClick={handleOnClick}
+        >
+            {children}
+        </span>
+    );
+};
+
+Label.propTypes = {
+    for: PropTypes.string,
+    children: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.node
+    ])
+};
+
+export default Label;
